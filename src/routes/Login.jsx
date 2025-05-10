bfcfa0ecba0927a261bc2f4f3cee0c9ad3fd1f07
2300031633 <152019439+vamsikrishna1324@users.noreply.github.com>
2025-05-11 02:42:45 +0530

    Update Login.jsx

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/Login.css';
+import '../styles/login.css';
 import logo from '/public/logo.png'; // Adjusted import path
 
 const Login = () => {
